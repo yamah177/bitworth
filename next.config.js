0860f5e4af0753282898ed1a70ba7d3ba10911b7
Cole Stuart <you@example.com>
2026-04-28 16:13:11 -0500

    fix: disable PWA service worker causing cache issues

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
+  disable: true,
+  register: false,
   skipWaiting: true,
-  buildExcludes: [/middleware-manifest\.json$/],
 })
 
 const nextConfig = {
